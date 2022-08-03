7091e4ab0137af924e7addb5b9b0ba1897c90644
quoc127 <nquoc160200@gmail.com>
2022-08-04 00:15:28 +0700

    add bootstrap and template

--- new-jdbc-12-month-2018/src/main/java/com/laptrinhjavaweb/controller/admin/HomeController.java
+++ new-jdbc-12-month-2018/src/main/java/com/laptrinhjavaweb/controller/admin/HomeController.java
@@ -2,6 +2,7 @@ package com.laptrinhjavaweb.controller.admin;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,9 +15,9 @@ public class HomeController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		RequestDispatcher rd = request.getRequestDispatcher("views/admin/home.jsp");
+		rd.forward(request, response);
 	}
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
--- new-jdbc-12-month-2018/src/main/java/com/laptrinhjavaweb/controller/web/HomeController.java
+++ new-jdbc-12-month-2018/src/main/java/com/laptrinhjavaweb/controller/web/HomeController.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.laptrinhjavaweb.model.UserModel;
+
 @WebServlet(urlPatterns = { "/trang-chu" })
 public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
